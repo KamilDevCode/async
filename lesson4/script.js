4088d109c64644a781f658861c05dc2de97b3d85
Kamil Lewandowski <kamlew555@gmail.com>
2024-04-10 20:20:17 +0200

    gettin know callback hell

--- lesson4/script.js
+++ lesson4/script.js
@@ -10,3 +10,20 @@ loadScript("./add.js", () => {
     console.log('script loaded')
     console.log(add(1, 2));
 })
+
+const runCalculation = () => {
+      const addResult = add(10, 10); // 20
+			const subtractResult = subtract(addResult, 4); // 16
+			const multiplyResult = multiply(subtractResult, 1); // 16
+			const divideResult = divide(multiplyResult, 2); // 8
+			console.log(divideResult);
+}
+
+
+loadScript("./add.js", () => {
+	loadScript("./subtract.js", () => {
+		loadScript("./multiply.js", () => {
+			loadScript("./divide.js", runCalculations);
+		});
+	});
+});
